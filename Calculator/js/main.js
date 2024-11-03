13d759e93bca5b6f39bde2eabce3daa0918cb712
Joshua Erney <57075505+JoshuaErney@users.noreply.github.com>
2024-11-03 18:43:55 -0500

    revised the js file to use switch statement

--- Calculator/js/main.js
+++ Calculator/js/main.js
@@ -22,187 +22,88 @@ const nineButton = document.querySelector("#nine");
 let num1 = "";
 let num2 = "";
 let operator = "";
-let result = 0;
+let result = "";
 
-function operate(num1, operator, num2) {
-  if (num1 !== "" && num2 !== "") {
-    if (operator === "+") {
-      result = parseInt(num1) + parseInt(num2);
-    } else if (operator === "-") {
-      result = parseInt(num1) - parseInt(num2);
-    } else if (operator === "*") {
-      result = parseInt(num1) * parseInt(num2);
-    } else if (operator === "/") {
-      result = parseInt(num1) / parseInt(num2);
-    } else if (operator === "%") {
-      result = (parseInt(num1) / 100) * parseInt(num2);
-    }
-    return result;
-  }
-  return result;
-}
-
-function nextEquation(result) {
-  num1 = result;
-  if (operator !== "" && num2 !== "") {
-    operate(num1, operator, num2);
-  } else {
-    return result;
-  }
-}
-
-/* ---------- Event Listeners ---------- */
-
-// Clear Button
-clearButton.addEventListener("click", () => {
+function clear() {
   display.value = "";
-  if (num1 !== "" && num2 !== "") {
-    num1 = "";
-    num2 = "";
+  num1 = "";
+  num2 = "";
+  operator = "";
+  result = "";
+}
+
+function addNumber(number) {
+  if (operator === "") {
+    num1 += number;
+  } else {
+    num2 += number;
   }
-  if (operator !== "") {
+  display.value += number;
+}
+
+function addOperator(op) {
+  if (num1 !== "") {
+    operator = op;
+    display.value += op;
+  }
+}
+
+function addDecimal() {
+  if (operator === "") {
+    if (!num1.includes(".")) {
+      num1 += ".";
+      display.value += ".";
+    }
+  } else {
+    if (!num2.includes(".")) {
+      num2 += ".";
+      display.value += ".";
+    }
+  }
+}
+
+function calculate() {
+  if (num1 !== "" && num2 !== "") {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch (operator) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+    }
+    display.value = result;
+    num1 = result;
+    num2 = "";
     operator = "";
   }
-});
+}
 
-// Add Button
-addButton.addEventListener("click", () => {
-  operator = "+";
-  display.value += "+";
-});
+// Event Listeners
+clearButton.addEventListener("click", clear);
+percentButton.addEventListener("click", () => addOperator("%"));
+divideButton.addEventListener("click", () => addOperator("/"));
+multiplyButton.addEventListener("click", () => addOperator("*"));
+subtractButton.addEventListener("click", () => addOperator("-"));
 
-// Subtract Button
-subtractButton.addEventListener("click", () => {
-  operator = "-";
-  display.value += "-";
-});
-
-// Multiply Button
-multiplyButton.addEventListener("click", () => {
-  operator = "*";
-  display.value += "*";
-});
-
-// Divide Button
-divideButton.addEventListener("click", () => {
-  operator = "/";
-  display.value += "/";
-});
-
-// Percent Button
-percentButton.addEventListener("click", () => {
-  operator = "%";
-  display.value += "%";
-});
-
-// Equals Button
-equalsButton.addEventListener("click", () => {
-  display.value = operate(num1, operator, num2);
-});
-
-// Decimal Button
-decimalButton.addEventListener("click", () => {
-  display.value += ".";
-});
-
-/* ---------- Number Buttons (0-9) ----------- */
-
-// Zero Button
-zeroButton.addEventListener("click", () => {
-  display.value += "0";
-  if (num1 !== "" && operator !== "") {
-    num2 += "0";
-  } else {
-    num1 += "0";
-  }
-});
-
-// One Button
-oneButton.addEventListener("click", () => {
-  display.value += "1";
-  if (num1 !== "" && operator !== "") {
-    num2 += "1";
-  } else {
-    num1 += "1";
-  }
-});
-
-// Two Button
-twoButton.addEventListener("click", () => {
-  display.value += "2";
-  if (num1 !== "" && operator !== "") {
-    num2 += "2";
-  } else {
-    num1 += "2";
-  }
-});
-
-// Three Button
-threeButton.addEventListener("click", () => {
-  display.value += "3";
-  if (num1 !== "" && operator !== "") {
-    num2 += "3";
-  } else {
-    num1 += "3";
-  }
-});
-
-// Four Button
-fourButton.addEventListener("click", () => {
-  display.value += "4";
-  if (num1 !== "" && operator !== "") {
-    num2 += "4";
-  } else {
-    num1 += "4";
-  }
-});
-
-// Five Button
-fiveButton.addEventListener("click", () => {
-  display.value += "5";
-  if (num1 !== "" && operator !== "") {
-    num2 += "5";
-  } else {
-    num1 += "5";
-  }
-});
-
-// Six Button
-sixButton.addEventListener("click", () => {
-  display.value += "6";
-  if (num1 !== "" && operator !== "") {
-    num2 += "6";
-  } else {
-    num1 += "6";
-  }
-});
-
-// Seven Button
-sevenButton.addEventListener("click", () => {
-  display.value += "7";
-  if (num1 !== "" && operator !== "") {
-    num2 += "7";
-  } else {
-    num1 += "7";
-  }
-});
-
-// Eight Button
-eightButton.addEventListener("click", () => {
-  display.value += "8";
-  if (num1 !== "" && operator !== "") {
-    num2 += "8";
-  } else {
-    num1 += "8";
-  }
-});
-
-// Nine Button
-nineButton.addEventListener("click", () => {
-  display.value += "9";
-  if (num1 !== "" && operator !== "") {
-    num2 += "9";
-  } else {
-    num1 += "9";
-  }
-});
+addButton.addEventListener("click", () => addOperator("+"));
+equalsButton.addEventListener("click", calculate);
+decimalButton.addEventListener("click", addDecimal);
+zeroButton.addEventListener("click", () => addNumber("0"));
+oneButton.addEventListener("click", () => addNumber("1"));
+twoButton.addEventListener("click", () => addNumber("2"));
+threeButton.addEventListener("click", () => addNumber("3"));
+fourButton.addEventListener("click", () => addNumber("4"));
+fiveButton.addEventListener("click", () => addNumber("5"));
+sixButton.addEventListener("click", () => addNumber("6"));
+sevenButton.addEventListener("click", () => addNumber("7"));
+eightButton.addEventListener("click", () => addNumber("8"));
+nineButton.addEventListener("click", () => addNumber("9"));
